63a4831ff6e378e1bdf8349c25c446f13259607f
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-05-25 21:27:58 +0500

    Создала репозиторий на аккаунте школы, создала массив ответов на вопрос вообращаемого пользователя и ункцию проверки ответов

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+const questionOne = {
+  question: 'Где обитает жираф?',
+  answer: {
+    a: 'В Индии',
+    b: 'В Америке',
+    c: 'В Африке',
+    d: 'В Австралии'
+  },
+  currectAnswer: 'В Африке'
+};
+const questionTwo = {
+  question: 'Как быстро может бегать жираф?',
+  answer: {
+    a: '70 км/ч',
+    b: '55 км/ч',
+    c: '30 км/ч',
+    d: '100 км/ч'
+  },
+  currectAnswer: '55 км/ч'
+};
+const questionThree = {
+  question: 'Сколько подвидов жирафа существует?',
+  answer: {
+    a: 9,
+    b: 7,
+    c: 10,
+    d: 17
+  },
+  currectAnswer: 9
+};
+const questionFour = {
+  question: 'Сколько живет жираф?',
+  answer: {
+    a: '50 лет',
+    b: '90 лет',
+    c: '30 лет',
+    d: '70 лет'
+  },
+  currectAnswer: '30 лет'
+};
+
+const questions = [questionOne, questionTwo, questionThree, questionFour];
+const userAnswer = ['В Африке', '30 км/ч', 9, '50 лет'];
+
+//Задание прошлого урока
+
+/* questions.map(obj => {
+  if (obj.currectAnswer === obj.answer.c) {
+    console.log(obj.currectAnswer);
+  }
+});
+ */
+
+function checkAnswer(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === questions[i].currectAnswer) {
+      console.log(`На вопрос '${questions[i].question}' вы ответили правильно!`);
+    } else {
+      console.log(`На вопрос '${questions[i].question}' ответ неверный :(`);
+    }
+  }
+}
+
+checkAnswer(userAnswer);
