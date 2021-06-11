9a532505f790bf66cbd5247ca0f14ad903865572
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-06-11 16:28:37 +0500

    Добавила формус валидайцией, подправила порядок функций

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,29 @@ const next = document.getElementById('next');
 const previous = document.getElementById('previous');
 const start = document.querySelector('.start-btn');
 const container = document.querySelector('.container');
+const form = document.querySelector('.form');
+const formName = document.querySelector('.form__name');
+const buttonName = document.querySelector('.form__btn');
+const regex = /(^[A-Z]{1}[a-z]{1,9}( )?$)|(^[А-Я]{1}[а-я]{1,9}( )?$)/;
 let countSlide = 0;
 let countAnswers = 0;
+
+start.addEventListener("click", () => {
+  start.classList.add('none');
+  next.classList.remove('none');
+  previous.classList.remove('none');
+  slides[0].classList.add('active');
+  console.log(countSlide + 1);
+})
+
+let assignHandler = () => {
+  Array.from(container.querySelectorAll('.question .question__answers'))
+    .forEach(answers => {
+      answers.addEventListener("click", checkResult);
+    })
+}
+
+assignHandler();
 
 function checkResult(event) {
   let target = event.target;
@@ -27,41 +48,16 @@ function checkResult(event) {
   event.currentTarget.removeEventListener("click", checkResult);
 }
 
-
-
-let assignHandler = () => {
-  Array.from(container.querySelectorAll('.question .question__answers'))
-    .forEach(answers => {
-      answers.addEventListener("click", checkResult);
-    })
-}
-
-assignHandler();
-
-function showResult() {
-  let counterAnswers = document.getElementById('counterAnswers');
-  counterAnswers.innerHTML = `Число правильных ответов ${countAnswers}`;
-}
-
-start.addEventListener("click", () => {
-  start.classList.add('none');
-  next.classList.remove('none');
-  previous.classList.remove('none');
-  slides[0].classList.add('active');
-  console.log(countSlide + 1);
-})
-
 next.addEventListener("click", () => {
   if (countSlide === 2) {
     next.textContent = 'check';
   }
+
   if (countSlide === 3) {
-    slides[3].classList.remove('active');
     next.classList.add('none');
     previous.classList.add('none');
-    showResult();
-    return;
   }
+  if (countSlide === 4) return;
   switchQuestion(countSlide + 1);
   countSlide++;
   console.log(countSlide + 1);
@@ -82,4 +78,23 @@ previous.addEventListener("click", () => {
 function switchQuestion(switchedSlide) {
   slides[countSlide].classList.remove('active');
   slides[switchedSlide].classList.add('active');
+}
+
+buttonName.addEventListener("click", function(event) {
+  let name = formName.value;
+  if (!regex.test(name)) {
+    let err = document.createElement('p');
+    err.classList.add('error');
+    err.textContent = 'Введите корректное имя(содержит 2-10 символов и начинается с заглавной буквы)';
+    form.append(err);
+    name = '';
+  } else {
+    slides[4].classList.remove('active');
+    showResult(name);
+  }
+})
+
+function showResult(name) {
+  let counterAnswers = document.getElementById('counterAnswers');
+  counterAnswers.innerHTML = `Число правильных ответов игрока ${name} ${countAnswers}`;
 }
