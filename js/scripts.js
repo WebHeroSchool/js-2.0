dfd6dc5ac210190cd372c37a1b45b1f09e50526d
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-06-14 14:57:43 +0500

    сделала таймер на каждый слайд, переделала функцию блокировки кнопок

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,13 @@ const buttonName = document.querySelector('.form__btn');
 const regex = /(^[A-Z]{1}[a-z]{1,9}( )?$)|(^[А-Я]{1}[а-я]{1,9}( )?$)/;
 let countSlide = 0;
 let countAnswers = 0;
+let isCorrect = null;
 
 start.addEventListener("click", () => {
   start.classList.add('none');
   next.classList.remove('none');
   previous.classList.remove('none');
-  slides[0].classList.add('active');
+  switchQuestion(countAnswers);
   console.log(countSlide + 1);
 })
 
@@ -29,15 +30,13 @@ assignHandler();
 
 function checkResult(event) {
   let target = event.target;
-
   if (target.tagName != 'LABEL') return;
 
-  const button = event.currentTarget.querySelectorAll('.radio');
-  button.forEach(item => item.setAttribute("disabled", "disabled"));
+  disableButton(event);
 
   const correctAnswers = ['В Африке', '55 км/ч', '9', '30 лет'];
   let answer = target.previousElementSibling.value;
-  let isCorrect = correctAnswers.includes(answer);
+  isCorrect = correctAnswers.includes(answer);
   if (isCorrect) {
     target.style.background = "green";
     countAnswers++;
@@ -46,9 +45,24 @@ function checkResult(event) {
   }
 
   event.currentTarget.removeEventListener("click", checkResult);
+
+  setTimeout(switchNextQuestion, 1000);
+  isCorrect = null;
 }
 
-next.addEventListener("click", () => {
+function disableButton(event) {
+  if (event) {
+    const button = event.currentTarget.querySelectorAll('.radio');
+    button.forEach(item => item.setAttribute("disabled", "disabled"));
+  } else {
+    const btn = slides[countSlide].querySelectorAll('.radio');
+    Array.from(btn).forEach(item => item.setAttribute("disabled", "disabled"));
+  }
+}
+
+next.addEventListener("click", switchNextQuestion);
+
+function switchNextQuestion() {
   if (countSlide === 2) {
     next.textContent = 'check';
   }
@@ -57,27 +71,43 @@ next.addEventListener("click", () => {
     next.classList.add('none');
     previous.classList.add('none');
   }
+
   if (countSlide === 4) return;
+
   switchQuestion(countSlide + 1);
   countSlide++;
   console.log(countSlide + 1);
-})
+}
 
-previous.addEventListener("click", () => {
+previous.addEventListener("click", switchPreviousQuestion);
+
+function switchPreviousQuestion() {
   if (countSlide <= 0) {
     return;
   }
+
   if (countSlide != 2) {
     next.textContent = 'next';
   }
+
   switchQuestion(countSlide - 1);
   countSlide--;
   console.log(countSlide + 1);
-})
+}
 
 function switchQuestion(switchedSlide) {
   slides[countSlide].classList.remove('active');
   slides[switchedSlide].classList.add('active');
+
+  let timer;
+  if (timer !== null) clearTimeout(timer);
+  if (countSlide < slides.length - 1 && isCorrect === null) {
+    timer = setTimeout(() => {
+      console.log('Таймер');
+      switchNextQuestion();
+      disableButton();
+    }, 10000);
+  }
 }
 
 buttonName.addEventListener("click", function(event) {
